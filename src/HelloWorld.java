1c5829a655fa2178bc77794bd9350613fccd308b
Cerissa Antill <cerissa.antill@gmail.com>
2019-03-19 15:45:26 -0500

    finished HelloWorld.java exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,32 @@
+public class HelloWorld {
+    public static void main(String[] arg) {
+        System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 17;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "This is a string";
+        System.out.println(myString);
+
+//        long myNumber = 123;
+//        System.out.println(myNumber);
+
+        float myNumber = 3.14f;
+        System.out.println(myNumber);
+
+
+//  prints the value of x and then increments
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+
+//  increments and then prints the value of x
+        int y = 5;
+        System.out.println(++y);
+        System.out.println(y);
+
+        System.out.println(Integer.MAX_VALUE);
+
+    }
+}
