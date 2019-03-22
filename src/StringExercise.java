99f767dfad1795a3373bfdd7dd8421226b25f10a
Cerissa Antill <cerissa.antill@gmail.com>
2019-03-22 09:51:35 -0500

    1 of 2 complete

---
+++ src/StringExercise.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class StringExercise {
+    public static void main(String[] args) {
+
+//  1.  String Basics. Create a class named StringExercise with a main method.
+//       - For each of the following output examples, create a string variable
+//           named message that contains the desired output and print it out to
+//           the console.
+
+//       - Do this with only one string variable and one print statement.
+//
+//
+//        We don't need no education
+//        We don't need no thought control
+//
+//        Check "this" out!, "s inside of "s!
+//
+//        In windows, the main drive is usually C:\
+//
+//        I can do backslashes \, double backslashes \\,
+//        and the amazing triple backslash \\\!
+
+
+    // adding spacing between exercises
+        System.out.println();
+
+
+        String message = "\nWe don't need no education.";
+        message = message + "\nWe don't need no thought control.\n";
+        message = message + "\nCheck \"this\" " + "out!" +", \"s inside of \"s!\n";
+        message = message + "\nIn windows, the main drive is usually C:\\\n";
+        message = message + "\nI can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
+
+        System.out.println(message);
+
+
+
+
+//  2.  Create a class named Bob with a main method for the following exercise.
+//
+//      Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+//
+//      Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
+//      He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
+//      He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
+//      He answers 'Whatever.' to anything else.
+//      Write the Java code necessary so that a user of your command line application can have a conversation with Bob.
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+    }
+}
