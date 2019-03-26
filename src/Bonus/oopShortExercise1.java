f99c0705a39639c5d42efdcbe1cac3634b19f724
Cerissa Antill <cerissa.antill@gmail.com>
2019-03-26 11:33:06 -0500

    renamed exercise

---
+++ src/Bonus/oopShortExercise1.java
@@ -0,0 +1,53 @@
+package Bonus;//package oopShortAssignment1;
+
+
+
+class RestaurantDish {
+
+    public int costInCents;
+    public String nameOfDish;
+    public boolean wouldRecommend;
+    public void eat() {
+        System.out.println("\nNom nom nom!\n");
+    }
+
+
+    public static void main(String[] args) {
+        RestaurantDish dish = new RestaurantDish();
+        dish.costInCents = 3000;
+        dish.nameOfDish = "\nPasta\n";
+        dish.wouldRecommend = true;
+        dish.eat();
+
+        System.out.printf("\n%d %s %b\n",
+                dish.costInCents,
+                dish.nameOfDish,
+                dish.wouldRecommend);
+
+    }
+    //  /main
+
+}
+//  /Bonus.RestaurantDish
+
+
+
+
+/*
+- OOP SHORT ASSIGNMENT #1 --
+
+        Create a class called "Bonus.RestaurantDish" (you should not include quotes)
+
+        Include an integer property called "costInCents"
+        Include a string property called "nameOfDish"
+        Include a boolean property called "wouldRecommend"
+        Include a method called "eat"
+        this method will print out "Nom nom nom!"
+
+        Create another class called RestaurantTest
+        Add a main method and inside the method...
+        1) instantiate a Bonus.RestaurantDish object called "dish"
+        2) assign creative values for each of the properties
+        3) print out each of the property values
+        4) test the eat() method by calling it
+*/
