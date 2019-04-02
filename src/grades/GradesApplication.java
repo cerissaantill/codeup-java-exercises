95d51f1356b9a021c2d7c363cd4c24488d285820
Cerissa Antill <cerissa.antill@gmail.com>
2019-04-01 20:36:34 -0500

    exercise 75% complete, GradesApplication unfinished

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,14 +1,95 @@
 package grades;
 import java.util.HashMap;
-
+import util.Input;
+import java.lang.String;
+import java.util.Map;
 
 public class GradesApplication {
 
     public static void main(String[] args) {
 
-        HashMap<String, String> Students = new HashMap<>();
+
+        Map<String, Student> students = new HashMap<>();
+
+        // import input class:
+            Input input = new Input();
 
 
+            Student student1 = new Student("Cerissa");
+            Student student2 = new Student("Katie");
+            Student student3 = new Student("Elizabeth");
+            Student student4 = new Student("Honey");
+
+
+            student1.addGrade(100);
+            student1.addGrade(92);
+            student1.addGrade(85);
+            student1.addGrade(95);
+
+
+            student2.addGrade(93);
+            student2.addGrade(100);
+            student2.addGrade(89);
+            student2.addGrade(82);
+
+
+            student3.addGrade(75);
+            student3.addGrade(86);
+            student3.addGrade(92);
+            student3.addGrade(83);
+
+
+            student4.addGrade(94);
+            student4.addGrade(89);
+            student4.addGrade(99);
+            student4.addGrade(81);
+
+
+
+            students.put(" cerissaantill", student1);
+            students.put(" katietottenham", student2);
+            students.put(" elizabethwalton", student3);
+            students.put(" honeybadger", student4);
+
+
+            ////////////////////////////////////////////////
+
+            System.out.println("Welcome\n");
+            System.out.println();
+            System.out.println("Here are the github usernames of our students: \n");
+
+            for (String key: students.keySet()) {
+                System.out.print(" | " + key);
+            }
+
+
+        System.out.println("\n\n ** OR enter \"ALL\" to view all student grades and class average");
+
+
+        do {
+            System.out.println("\n\nWhich user would you like information on? \nEnter username below: ");
+                System.out.println("> ");
+            String userInput = input.getString();
+
+
+            if (students.keySet().contains(userInput)) {
+
+                System.out.println("\nInformation for: " + userInput + "\n");
+                System.out.println("Name: " + students.get(userInput).getName() + "\nGithub username: " + userInput);
+                System.out.println("Current Average: " + students.get(userInput).getAverage());
+                System.out.println("All Grades: " + students.get(userInput).getGrades());
+
+            }  else {
+                System.out.println("Sorry, no student found with the github username of " + userInput);
+                System.out.println("\nEnter a valid username to view information:\n");
+                userInput = input.getString();
+                System.out.print("> " + userInput);
+            }
+
+        } while (input.yesNo("\nWould you like to view another user?"));
+
+
+            System.out.println("\nGoodbye");
 
 
 
--- src/grades/Student.java
+++ src/grades/Student.java
@@ -22,8 +22,9 @@ public class Student {
 
     // returns the student's name
     public String getName() {
-       return name;
+       return this.name;
     }
+
 
 
     // adds the given grade to the grades property
@@ -32,21 +33,23 @@ public class Student {
     }
 
 
-    // returns the average of the student's grades
-    public double getGradeAverage() {
-       return gradeAverage;
-    }
 
 
+// //    returns the average of the student's grades
+//    public double getGradeAverage() {
+//       double total = 0;
+//       for (int i = 0; i<this.grades.size(); i++) {
+//           total += grade;
+//       }
+//    }
 
 
+//
+//    // grades Getter
+//    public ArrayList<Integer> getGrades() {
+//        return grades;
+//    }
 
-
-
-    // grades Getter
-    public ArrayList<Integer> getGrades() {
-        return grades;
-    }
 
 
     // returns the average of the students grades
@@ -54,30 +57,72 @@ public class Student {
         if (grades.size() == 0) {
             return 0;
         } else {
-            grades = getGrades();
+//            grades = getGrades();
             int sum = 0;
             for (int grade : grades) {
                 sum += grade;
             }
-            return sum / grades.size();
+            return sum / this.grades.size();
         }
     }
 
 
 
-
-
-
+// MAIN method:
     public static void main(String[] args) {
-        System.out.println();
+
+    // create Student objects, add grades to each,
+    //  make sure getGradeAverage method correctly returns student's avg
+
+        Student student1 = new Student("Cerissa");
+            student1.addGrade(100);
+            student1.addGrade(92);
+            student1.addGrade(85);
+            student1.addGrade(95);
+
+        System.out.println("Name: " + student1.getName() + "\n"
+                + "Average: " + student1.getAverage()  + "\n"
+                + "All Grades: " + student1.grades + "\n"
+                +  "------------------------------------");
 
 
 
+        Student student2 = new Student("Katie");
+            student2.addGrade(93);
+            student2.addGrade(100);
+            student2.addGrade(89);
+            student2.addGrade(82);
+
+        System.out.println("Name: " + student2.getName() + "\n"
+                + "Average: " + student2.getAverage()  + "\n"
+                + "All Grades: " + student2.grades + "\n"
+                +  "------------------------------------");
 
 
 
+        Student student3 = new Student("Elizabeth");
+            student3.addGrade(75);
+            student3.addGrade(86);
+            student3.addGrade(92);
+            student3.addGrade(83);
+
+        System.out.println("Name: " + student3.getName() + "\n"
+                + "Average: " + student3.getAverage()  + "\n"
+                + "All Grades: " + student3.grades + "\n"
+                +  "------------------------------------");
 
 
+
+        Student student4 = new Student("Honey");
+            student4.addGrade(94);
+            student4.addGrade(89);
+            student4.addGrade(99);
+            student4.addGrade(81);
+
+        System.out.println("Name: " + student4.getName() + "\n"
+                + "Average: " + student4.getAverage()  + "\n"
+                + "All Grades: " + student4.grades + "\n"
+                +  "------------------------------------");
 
 
 
