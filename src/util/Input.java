2e19e3deeb205fe61fd30e00a660168e53ea1a05
Cerissa Antill <cerissa.antill@gmail.com>
2019-04-03 11:01:46 -0500

    refactored for exceptions & error handling (try/catch)

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,115 +1,106 @@
 package util;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-
-// create a class named Input that has a private property named Scanner.
 public class Input {
+    Scanner scanner;
 
-    private Scanner scanner;
-
-
+    // constructor returns an Input object
     public Input() {
-        this.scanner = new Scanner(System.in);
+        scanner = new Scanner(System.in).useDelimiter("\n");
     }
 
-
-
-    // create String getString() method that return command line  user input
-
-    public String getString() {
-        String input = scanner.nextLine();
-        return input;
+    public boolean yesNo() {
+        String input = getString();
+        return input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes");
     }
 
-
-
-    public boolean yesNo(){
-        String input = scanner.next();
-        if (input.equalsIgnoreCase("yes") || input.equalsIgnoreCase("y") || input.contains("yes")) {
-            return true;
-        } return false;
+    public boolean yesNo(String prompt) {
+        System.out.println(prompt);
+        return yesNo();
     }
-
-//
-//    public boolean yesNo(String prompt){
-//        System.out.println(prompt);
-//        String input = scanner.next();
-//        if (input.equalsIgnoreCase("yes") || input.equalsIgnoreCase("y") || input.contains("yes")) {
-//            return true;
-//        } return false;
-//    }
-
-
-
-
-
-    // int getInt() method ...
 
     public int getInt() {
-        System.out.println("\nEnter a number: \n");
-        String input = scanner.next();
-        if (input.equalsIgnoreCase("yes" || input.equalsIgnoreCase("y") || input.contains("yes")) {
+        // we need to gracefully handle exceptions
+        String userInput;
 
+        try {
+            // get input from the user
+            // parse the input string as an integer
+            // if valid, return the input
+            userInput = scanner.next();
+            return Integer.valueOf(userInput);
+
+        } catch (Exception e) {
+            // catch runs if the "try" code throws an exception
+            System.out.println("Please make sure your input is a valid integer using numerals");
+            return getInt();
         }
 
-
-
-
-//        if (scanner.hasNextInt()) {
-//            return scanner.nextInt();
-//        }
-//        scanner.next();
-//        return getInt();
     }
 
+    public int getInt(String prompt) {
+        System.out.println(prompt);
+        return getInt();
+    }
 
-
-
-    // int getInt(int min, int max) method...
-
-    int getInt(int min, int max) {
-
+    public int getInt(int min, int max) {
         int userInt = getInt();
-        if (userInt >= min && userInt <= max) {
+        if(userInt >= min && userInt <= max) {
             return userInt;
+        } else {
+            System.out.println("The integer must be between " + min + " and " + max);
+            return getInt(min, max);
         }
-        scanner.nextInt();
+    }
+
+    public int getInt(int min, int max, String prompt) {
+        System.out.println(prompt);
         return getInt(min, max);
     }
 
+    public String getString() {
+        return scanner.next();
+    }
 
+    public String getString(String prompt) {
+        System.out.println(prompt);
+        return getString();
+    }
 
     public double getDouble() {
-        if (scanner.hasNextDouble()) {
-            return scanner.nextDouble();
+        String userInput;
+
+        try {
+            userInput = scanner.next();
+            return Double.valueOf(userInput);
+
+        } catch (Exception e) {
+            System.out.println("Please input a valid double");
+            return getDouble();
         }
-        scanner.next();
+    }
+
+    public double getDouble(String prompt) {
+        System.out.println(prompt);
         return getDouble();
     }
 
-
-
-    double getDouble(double min, double max) {
+    public double getDouble(double min, double max) {
         double userDouble = getDouble();
-        if (userDouble >= min && userDouble <= max) {
+        if(userDouble >= min && userDouble <= max) {
             return userDouble;
+        } else {
+            System.out.println("The double must be between " + min + " and " + max);
+            return getDouble(min, max);
         }
-        scanner.next();
+    }
+
+    public double getDouble(double min, double max, String prompt) {
+        System.out.println(prompt);
         return getDouble(min, max);
     }
 
 
-
-
-
-    public static void main(String[] args) {
-        Input in = new Input();
-        System.out.println("Enter yes or no [y/n]");
-        System.out.println(in.yesNo());
-
-        System.out.println(in.getInt(2, 4));
-    }
-
-
-}
-// Input class at the top
+}   // Input class
